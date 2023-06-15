7c567dcec13b89584cd5eb10fc29fe7ff35df831
Maitreya-today <lessismoreclint@gmail.com>
2023-06-15 10:18:16 -0500

    update to camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,30 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class CameraController : MonoBehaviour
 {
-
     public GameObject player;
-
-    [SerializeField] Vector3 offset = Vector3.zero;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (player == null)
-        {
-            Abort("CameraController: player is null");
-            return;
-        }
-        offset = transform.position - player.transform.position;
-    }
-
+    public Vector3 cameraOffset = new Vector3(0f, 20f, -10f);
     // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        if (player != null && offset != Vector3.zero)
-            transform.position = player.transform.position + offset;
+        if (player != null)
+        {
+            transform.position = player.transform.position + cameraOffset;
+        }
     }
-}
