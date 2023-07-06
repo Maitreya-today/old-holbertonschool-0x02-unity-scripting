18cdf28982437761e033f3c5a859d4ce48a98ee5
Maitreya-today <lessismoreclint@gmail.com>
2023-07-06 10:10:43 -0500

    player script updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,62 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
     private int score = 0;
     public int health = 5;
+    public Rigidbody m_Rigidbody;
+    public float speed = 700;
 
-    private Rigidbody playerRigidBody;
-    private void Start()
-    {
-        playerRigidBody = GetComponent<Rigidbody>();
-    }
+    // Start is called before the first frame update
+    void Start()
+    {}
 
     // Update is called once per frame
-    private void FixedUpdate()
+    void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
-        playerRigidBody.velocity = movement * speed;
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+            m_Rigidbody.AddForce(speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("a"))
+            m_Rigidbody.AddForce(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("w"))
+            m_Rigidbody.AddForce(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("s"))
+            m_Rigidbody.AddForce(0, 0, -speed * Time.deltaTime);
     }
 
-    private void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Pickup"))
+        if (other.gameObject.tag == "Trap")
         {
-            score++;
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
+        if (other.gameObject.tag == "Pickup")
+        {
+            score += 1;
             Debug.Log("Score: " + score);
             Destroy(other.gameObject);
         }
-        else if (other.CompareTag("Trap"))
-        {
-            health--;
-            Debug.Log("Health: " + health);
-
-            if (health <= 0)
-            {
-                GameOver();
-            }
-        }
-        else if (other.CompareTag("Goal"))
+        if (other.gameObject.tag == "Goal")
         {
             Debug.Log("You win!");
         }
     }
-    private void Update()
-    {
-        if (health <=0 )
-        {
-            GameOver();
-        }
-    }
 
-    private void GameOver()
+    void ZeroHealth(Collider other)
     {
-        Debug.Log("Game Over!");
-        score = 0;
-        health = 5;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
